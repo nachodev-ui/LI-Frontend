190c3f69e4e0a1d236114907436c5f7372a4a5a4
nachodev-ui <ig.cisternas@duocuc.cl>
2023-04-20 06:39:56 -0400

    deleted: some hrefs

--- data/ceo.js
+++ data/ceo.js
@@ -2,13 +2,14 @@ import Head from "next/head"
 
 const ceo = page => {
   return (
-    <>
+    <div>
         <Head>
-            <title>Librería Imagina - {page}</title>
+            <title>Librería Imagina - {page} </title>
             <meta name="description" content="Integración Librería Imagina por TESS" />
+            <link rel="icon" href="/img/zyro-image.png" />
         </Head>
-    </>
+    </div>
   )
 }
 
-export default ceo
+export default ceo;
