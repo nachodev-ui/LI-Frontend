a0e849f4c2e6b7d2bce3ae8f64e7f14913d5d5b3
nachodev-ui <ig.cisternas@duocuc.cl>
2023-04-21 03:47:34 -0400

    add: Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,56 @@
+import { Typography } from "@material-tailwind/react";
+import Image from "next/image";
+ 
+const Footer = () => {
+  return (
+    <footer className="w-full bg-white p-8">
+      <div className="footer flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
+        <Image src="/img/zyro-image.png" width={70} height={100} alt="logo-ct" />
+        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              About Us
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              License
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              Contribute
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              Contact Us
+            </Typography>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
