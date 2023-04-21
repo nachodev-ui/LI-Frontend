2116af145afc894a3b21bf754029ac0b9401299e
nachodev-ui <ig.cisternas@duocuc.cl>
2023-04-21 03:53:36 -0400

    add: /historial

--- data/links.js
+++ data/links.js
@@ -1,8 +1,9 @@
-import { HomeIcon, UserIcon } from "@heroicons/react/24/solid";
+import { HomeIcon, UserIcon, DocumentTextIcon } from "@heroicons/react/24/solid";
 
 const links = [
     { href: '/', text: 'Inicio', icon: <HomeIcon className="w-4 h-4" /> },
     { href: '/perfil', text: 'Perfil', icon: <UserIcon className="w-4 h-4" />},
+    { href: '/historial', text: 'Mi historial', icon: <DocumentTextIcon className="w-4 h-4" /> },
 ];
 
 export default links;
