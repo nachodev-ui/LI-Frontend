9372f27acf17f756339bb408f1fb62f7dfa90135
nachodev-ui <ig.cisternas@duocuc.cl>
2023-04-21 03:57:12 -0400

    add: Navbar to libros.js

--- pages/libros.js
+++ pages/libros.js
@@ -1,9 +1,12 @@
 import ceo from "@/data/ceo"
+import ComplexNavbar from "@/components/Navbar"
 
 const Libros = () => {
   return (
     <div>
       {ceo("Libros")}
+
+      <ComplexNavbar />
 
       <p className="text-gray-700 text-3xl mb-16 font-bold">Libros</p>
     </div>
