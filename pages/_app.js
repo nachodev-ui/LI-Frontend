c399467e7a32f03f0154a55620e73622b21fdd9a
nachodev-ui <ig.cisternas@duocuc.cl>
2023-04-21 03:54:12 -0400

    add: google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,10 @@ import { ThemeProvider } from '@material-tailwind/react';
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> 
+      <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600;700&display=swap" rel="stylesheet" />
+      
       <Component {...pageProps} />
     </ThemeProvider>
   );
