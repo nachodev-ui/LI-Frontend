c5ac5c481cb7ff19785a3f61a2cad04f20bebaba
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-15 15:32:06 -0400

    add: showPassword function

--- components/forms/LoginForm.jsx
+++ components/forms/LoginForm.jsx
@@ -26,11 +26,16 @@ const validate = (values) => {
 const LoginForm = () => {
   const [error, setError] = useState(null);
   const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
+  const [showPassword, setShowPassword] =  useState(false);
   const router = useRouter();
 
   const handleCloseErrorModal = () => {
     setIsErrorModalOpen(false);
   };
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
 
   const onSubmit = async (values) => {
 
@@ -86,7 +91,7 @@ const LoginForm = () => {
       <div className="relative">
         <input
           className="text-sm p-2 rounded-xl border w-full"
-          type="password"
+          type={showPassword ? "text" : "password"}
           name="password"
           placeholder="Contraseña"
           value={values.password}
@@ -104,6 +109,7 @@ const LoginForm = () => {
           height="16"
           fill="gray"
           viewBox="0 0 16 16"
+          onClick={toggleShowPassword}
         >
           <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
           <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
