70e0fab13bedb59bf2445fb5f8e01ba77ebbe54a
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-30 09:29:36 -0400

    updated: filteredBooks fixed in Main

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,49 +1,47 @@
-import { useState, useEffect } from "react";
-import Books from "./Books";
-import axios from "axios";
-import { useFilters } from "@/hooks/useFilters";
+import { useState, useEffect } from 'react'
+import Books from './Books'
+import axios from 'axios'
+import { useFilters } from '@/hooks/useFilters'
 
 const Main = () => {
-  const [books, setBooks] = useState([]);
-  const { filters, filterBooks } = useFilters();
-  const filteredBooks = filterBooks(books);
+  const [books, setBooks] = useState([])
+  const { filterBooks } = useFilters()
+  const filteredBooks = filterBooks(books)
 
   const fetchBooks = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/books");
+      const response = await axios.get('http://localhost:5000/api/books')
       if (Array.isArray(response.data.data)) {
         const booksData = response.data.data.map((book) => {
-          const precio = parseFloat(book.precio);
+          const precio = parseFloat(book.precio)
           return {
             ...book,
             precio: Number.isFinite(precio) ? precio : 0,
-          };
-        });
-        setBooks(booksData);
+          }
+        })
+        setBooks(booksData)
       } else {
         console.log(
-          "La respuesta de la API no es un array válido:",
+          'La respuesta de la API no es un array válido:',
           response.data
-        );
+        )
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchBooks();
-  }, []);
+    fetchBooks()
+  }, [])
 
   return (
     <div className="mx-auto flex items-center flex-wrap pb-12">
       <div className="w-full xl:px-20 px-4 container items-center justify-between mt-0 py-3 mx-auto">
-        <p className="font-bold text-gray-800 text-4xl">Lo más destacado</p>
+        <p className="font-bold text-gray-800 text-4xl mb-8">
+          Lo más destacado
+        </p>
       </div>
-
-      {/* <div className="mx-auto">
-        <FiltersBar />
-      </div> */}
 
       <div className="w-full mx-auto px-6">
         <div className="flex flex-wrap justify-center">
@@ -51,7 +49,7 @@ const Main = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
