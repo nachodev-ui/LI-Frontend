044cacc4c64119ee34ba3e8b345294bb2347d91f
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-30 09:42:14 -0400

    no-update: nothing changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@material-tailwind/react'
 import { FiltersProvider } from '@/context/filters'
-import '../styles/globals.css'
 import { CartProvider } from '@/context/cart'
+import '../styles/globals.css'
 
 const App = ({ Component, pageProps }) => {
   return (
