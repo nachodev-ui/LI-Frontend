9ca661909dfc51173349d63b128da289a1003cb9
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-30 09:41:14 -0400

    no-update: nothing changed

--- hooks/useFilters.js
+++ hooks/useFilters.js
@@ -1,17 +1,17 @@
-import { useContext } from "react";
-import { FiltersContext } from "@/context/filters";
+import { useContext } from 'react'
+import { FiltersContext } from '@/context/filters'
 
 export function useFilters() {
-   const { filters, setFilters } = useContext(FiltersContext);
+  const { filters, setFilters } = useContext(FiltersContext)
 
   const filterBooks = (books) => {
     return books.filter((book) => {
       return (
         book.precio >= filters.minPrice &&
-        (filters.genre === "all" || book.genero === filters.genre)
-      );
-    });
-  };
+        (filters.genre === 'all' || book.genero === filters.genre)
+      )
+    })
+  }
 
-  return { filters, filterBooks, setFilters };
+  return { filters, filterBooks, setFilters }
 }
