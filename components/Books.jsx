81f614ad10ccb42e7133213e165da7056ad1ecfe
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-30 09:23:24 -0400

    add: Sonner to alerts in Books

--- components/Books.jsx
+++ components/Books.jsx
@@ -7,19 +7,28 @@ import {
   Typography,
   Button,
 } from '@material-tailwind/react'
+import { formatPrice } from '@/utils/cartUtils'
+import { Toaster, toast } from 'sonner'
 
 const Books = ({ books }) => {
   const { addToCart } = useCart()
 
+  const handleSonnerAddToCart = async () => {
+    toast.success('Añadido al carro', {
+      position: 'bottom-right',
+      duration: 5000,
+    })
+  }
+
   return (
     <>
       {books.map((book) => (
-        <div className="grid grid-cols-1 px-8 my-12" key={book.id}>
-          <Card className="w-60 h-90 bg-[#f8f8f8]" key={book.id}>
+        <div className="px-8 my-12" key={book.id}>
+          <Card className="w-60 h-90 bg-[#f8f8f8]">
             <CardHeader shadow={true} floated={true} className="rounded-sm">
               <div
                 className="w-full h-0"
-                style={{ paddingBottom: '133.33%' }} // 3:4 aspect ratio
+                style={{ paddingBottom: '133.33%' }} // 3:4
               >
                 <img
                   src={book.imagen}
@@ -35,7 +44,7 @@ const Books = ({ books }) => {
                 </Typography>
               </div>
               <Typography className="text-sm text-gray-800 opacity-75">
-                {book.descripcion}
+                Género: {book.genero}
               </Typography>
               <Typography
                 variant="small"
@@ -44,17 +53,28 @@ const Books = ({ books }) => {
               >
                 Stock: {book.stock}
               </Typography>
-              <Typography className="font-bold mt-4">${book.precio}</Typography>
+              <Typography className="text-lg mt-4 text-gray-800">
+                {formatPrice(book.precio)}
+              </Typography>
             </CardBody>
             <CardFooter className="pt-0">
-              <Button
-                fullWidth={true}
-                className="bg-[#F6C38C] shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-                onClick={() => addToCart(book)}
-                {...books}
-              >
-                <span className="text-white">Añadir al carro</span>
-              </Button>
+              <Toaster />
+              {book.stock > 0 ? (
+                <Button
+                  fullWidth={true}
+                  className="bg-[#313131] shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
+                  onClick={() => {
+                    addToCart(book)
+                    handleSonnerAddToCart()
+                  }}
+                >
+                  <span className="text-white">Añadir al carro</span>
+                </Button>
+              ) : (
+                <Button fullWidth={true} className="bg-[#212121]" disabled>
+                  <span className="text-white">Sin stock</span>
+                </Button>
+              )}
             </CardFooter>
           </Card>
         </div>
