de6c18103ae9c005699afba91269d64b19706a83
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-30 09:39:21 -0400

    add: 'Mis compras' to links array

--- data/links.js
+++ data/links.js
@@ -1,9 +1,31 @@
-import { HomeIcon, UserIcon, DocumentTextIcon } from "@heroicons/react/24/solid";
+import {
+  HomeIcon,
+  UserIcon,
+  DocumentTextIcon,
+  ShoppingBagIcon,
+} from '@heroicons/react/24/solid'
 
 const links = [
-    { href: '/', text: 'Inicio', icon: <HomeIcon className="w-4 h-4" /> },
-    { href: '/perfil', text: 'Perfil', icon: <UserIcon className="w-4 h-4" />},
-    { href: '/historial', text: 'Mi historial', icon: <DocumentTextIcon className="w-4 h-4" /> },
-];
+  {
+    href: '/',
+    text: 'Inicio',
+    icon: <HomeIcon className="w-4 h-4" />,
+  },
+  {
+    href: '/perfil',
+    text: 'Perfil',
+    icon: <UserIcon className="w-4 h-4" />,
+  },
+  {
+    href: '/historial',
+    text: 'Mantenciones',
+    icon: <DocumentTextIcon className="w-4 h-4" />,
+  },
+  {
+    href: '/compras',
+    text: 'Mis compras',
+    icon: <ShoppingBagIcon className="w-4 h-4" />,
+  },
+]
 
-export default links;
+export default links
