ed4c82c0c974290afd127f5f56683e6a8891b209
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-30 09:27:04 -0400

    updated: useEffect for getGenres

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,65 +1,50 @@
-import { useId } from "react";
-import { useFilters } from "@/hooks/useFilters";
-import { Select, Option } from "@material-tailwind/react";
+import { useId } from 'react'
+import { useFilters } from '@/hooks/useFilters'
+import { Select, Option } from '@material-tailwind/react'
+import { useBooks } from '@/hooks/useBooks'
+import { useEffect, useState } from 'react'
 
 export function Filters() {
-  const { filters, setFilters } = useFilters();
+  const { setFilters } = useFilters()
+  const { books } = useBooks()
+  const [uniqueGenres, setUniqueGenres] = useState([])
 
-  const minPriceFilterId = useId();
-  const genreFilterId = useId();
+  useEffect(() => {
+    const getUniquesGenres = (books) => {
+      const genres = books.map((book) => book.genero)
+      return [...new Set(genres)]
+    }
 
-  const handleChangeMinPrice = (event) => {
+    setUniqueGenres(getUniquesGenres(books))
+  }, [books])
+
+  const genreFilterId = useId()
+
+  const handleChangeGenre = (value) => {
     setFilters((prevState) => ({
       ...prevState,
-      minPrice: event.target.value,
-    }));
-  };
-
-  const handleChangeGenre = (event) => {
-    setFilters((prevState) => ({
-      ...prevState,
-      genre: event.target.value,
-    }));
-  };
+      genre: value,
+    }))
+  }
 
   return (
-    <section>
-      {/* <div className="flex gap-4 mx-8">
-        <label htmlFor={minPriceFilterId}>Precio mínimo:</label>
-        <input
-          type="range"
-          min="0"
-          max="50000"
-          id={minPriceFilterId}
-          onChange={handleChangeMinPrice}
-          value={filters.minPrice}
-        />
-        <span>${filters.minPrice}</span>
-      </div>
-      <div className="">
-        <label htmlFor={genreFilterId}>Genero:</label>
-        <select id={genreFilterId} onChange={handleChangeGenre}>
-          <option value="all">All</option>
-          < value="Fantasía">Fantasía</ option>
-          <option value="romance">Romance</option>
-          <option value="Terror">Terror</option>
-        </select>
-      </div> */}
-
+    <section className="flex flex-row">
       <label htmlFor={genreFilterId}></label>
-      <div className="flex w-78 flex-col gap-6 mt-8 px-8">
+      <div className="w-80 gap-6 mt-8 px-8">
         <Select
           id={genreFilterId}
           onChange={handleChangeGenre}
           color="purple"
           label="Selecciona un género"
         >
-          <Option value="all">Todos</Option>
-          <Option value="Fantasía">Fantasía</Option>
-          <Option value="Romance">Romance</Option>
-          <Option value="Terror">Terror</Option>
+          <Option value="all">Todos los géneros</Option>
+          {uniqueGenres.map((genre, index) => (
+            <Option key={index} value={genre}>
+              {genre}
+            </Option>
+          ))}
         </Select>
       </div>
     </section>
-  );
+  )
 }
