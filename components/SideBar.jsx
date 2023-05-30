640a4baa66d091cbbcccdde7be2f6ad3676453a1
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-30 09:33:48 -0400

    no-updated: nothing changed

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,10 +1,10 @@
-import { forwardRef } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import links from "@/data/links";
+import { forwardRef } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import links from '@/data/links'
 
 const SideBar = forwardRef(({ showNav }, ref) => {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
     <div ref={ref} className="fixed w-56 h-full bg-white shadow-sm">
@@ -18,32 +18,29 @@ const SideBar = forwardRef(({ showNav }, ref) => {
         </picture>
       </div>
 
-      {links.map( links =>  (
+      {links.map((links) => (
         <div className="flex flex-col" key={links.href}>
           <Link href={links.href}>
             <div
               className={`pl-6 py-3 mx-4 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
                 router.pathname === links.href
-                ? "bg-gray-200 text-gray-700 font-bold"
-                : "text-gray-400 hover:text-gray-600 hover:bg-gray-100"
-              }`} 
-            > 
+                  ? 'bg-gray-200 text-gray-700 font-bold'
+                  : 'text-gray-400 hover:text-gray-600 hover:bg-gray-100'
+              }`}
+            >
+              {links.icon}
 
-             {links.icon}
-              
               <div className="ml-2">
                 <p>{links.text}</p>
               </div>
-
             </div>
           </Link>
         </div>
       ))}
-
     </div>
-  );
-});
+  )
+})
 
-SideBar.displayName = "SideBar";
+SideBar.displayName = 'SideBar'
 
-export default SideBar;
+export default SideBar
