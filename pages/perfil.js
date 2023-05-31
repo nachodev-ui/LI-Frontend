f7cfac25cfa419e21ef4a8d14a1604f9be10538d
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-31 01:16:41 -0400

    updated: added background dark mode

--- pages/perfil.js
+++ pages/perfil.js
@@ -1,14 +1,26 @@
 import Ceo from '@/components/Ceo'
 import ProfileLayout from '@/components/ProfileLayout'
 import UserProfile from '@/components/UserProfile'
+import { useDark } from '@/hooks/useDark'
+
+const colors = {
+  light: 'bg-gray-100',
+  dark: 'bg-gray-800',
+}
 
 const perfil = () => {
-  return (
-    <ProfileLayout>
-      <Ceo page="Perfil" />
+  const { darkMode } = useDark()
 
-      <UserProfile />
-    </ProfileLayout>
+  const color = darkMode ? colors.dark : colors.light
+
+  return (
+    <div className={`${color.dark} min-h-screen`}>
+      <ProfileLayout>
+        <Ceo page="Perfil" />
+
+        <UserProfile />
+      </ProfileLayout>
+    </div>
   )
 }
 
