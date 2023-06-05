9dfd93ccd77fc629804d8d738dd57b93875c5b7b
nachodev-ui <ig.cisternas@duocuc.cl>
2023-06-05 09:08:40 -0400

    updated: add Sonner alerts

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -3,22 +3,66 @@ import Example from './Navbar'
 import CartSteps from './CartSteps'
 import Shipping from './Shipping'
 import ShippingMethod from './ShippingMethod'
+import Swal from 'sweetalert2'
 import { formatPrice } from '../utils/cartUtils'
 import { TrashIcon } from '@heroicons/react/24/outline'
 import { useCart } from '@/hooks/useCart'
 import { useEffect } from 'react'
+import { Toaster, toast } from 'sonner'
 
 export function Cart() {
   const { cart, removeFromCart, addToCart, clearCart } = useCart()
+  const router = Router
+
+  const needLoginAlert = () => {
+    Swal.fire({
+      title: 'Debes iniciar sesión para continuar',
+      icon: 'warning',
+      iconColor: '#F87171',
+      showCancelButton: true,
+      confirmButtonText: `Iniciar sesión`,
+      confirmButtonColor: '#F87171',
+      cancelButtonText: `Cancelar`,
+      cancelButtonColor: '#9CA3AF',
+      showCloseButton: false, // Desactiva el botón de cerrar en la esquina superior derecha
+      allowOutsideClick: false, // Evita que se pueda hacer clic fuera de la alerta para cerrarla
+    }).then((result) => {
+      if (result.isConfirmed) {
+        router.push('/login')
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        router.push('/')
+      }
+    })
+  }
 
   useEffect(() => {
     const authToken = localStorage.getItem('authToken')
-    const router = Router
 
     if (!authToken) {
-      router.push('/login')
+      needLoginAlert()
     }
   })
+
+  const handleSonnerAddToCart = async () => {
+    toast.success('Libro agregado correctamente', {
+      position: 'bottom-right',
+      duration: 5000,
+    })
+  }
+
+  const handleSonnerRemoveFromCart = async () => {
+    toast.success('Libro eliminado correctamente', {
+      position: 'bottom-right',
+      duration: 5000,
+    })
+  }
+
+  const handleSonnerClearFromCart = async () => {
+    toast.success('Carro de compras limpiado correctamente', {
+      position: 'bottom-right',
+      duration: 5000,
+    })
+  }
 
   return (
     <>
@@ -61,9 +105,13 @@ export function Cart() {
                       {formatPrice(book.precio)}
                     </p>
                     <span className="inline-flex mt-8 text-gray-500">
+                      <Toaster />
                       <button
                         className="h-6 w-6 flex items-center justify-center rounded-md"
-                        onClick={() => removeFromCart(book)}
+                        onClick={() => {
+                          removeFromCart(book)
+                          handleSonnerRemoveFromCart()
+                        }}
                       >
                         <svg
                           className="h-4 w-4 text-gray-600"
@@ -80,9 +128,13 @@ export function Cart() {
                       <p className="rounded-sm px-2 text-gray-600">
                         Cantidad: {book.quantity}
                       </p>
+                      <Toaster />
                       <button
                         className="h-6 w-6 flex items-center justify-center rounded-md"
-                        onClick={() => addToCart(book)}
+                        onClick={() => {
+                          addToCart(book)
+                          handleSonnerAddToCart()
+                        }}
                       >
                         <svg
                           className="h-4 w-4 text-gray-800"
@@ -100,9 +152,11 @@ export function Cart() {
                   </div>
 
                   <div className="flex-row mt-4">
+                    <Toaster />
                     <button
                       onClick={() => {
                         clearCart(book)
+                        handleSonnerClearFromCart()
                       }}
                     >
                       <TrashIcon className="sm:flex-col h-5 w-5 text-gray-600" />
