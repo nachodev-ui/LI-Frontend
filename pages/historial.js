28256a70fea72f4d2b7552a385adc69d4bd440b6
nachodev-ui <ig.cisternas@duocuc.cl>
2023-06-05 09:29:54 -0400

    updated: dark mode & added MantencionesList(by user)

--- pages/historial.js
+++ pages/historial.js
@@ -1,12 +1,29 @@
 import ProfileLayout from '@/components/ProfileLayout'
 import Ceo from '@/components/Ceo'
+import MantencionesList from '@/components/lists/MantencionesList'
+import { useDark } from '@/hooks/useDark'
 
-const historial = () => {
+const colors = {
+  light: {
+    background: 'bg-[#f7f7f7]',
+  },
+  dark: {
+    background: 'bg-[#212121]',
+  },
+}
+
+const historial = ({ showNav }) => {
+  const { darkMode } = useDark()
+
+  const color = darkMode ? colors.dark : colors.light
+
   return (
-    <div>
+    <div className={`${color.background}`}>
       <Ceo page="Mis manteciones" />
 
-      <ProfileLayout />
+      <ProfileLayout showNav={showNav}>
+        <MantencionesList />
+      </ProfileLayout>
     </div>
   )
 }
