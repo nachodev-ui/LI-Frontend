504f2ea945dd0265aa656c096189ef9b665e853b
nachodev-ui <ig.cisternas@duocuc.cl>
2023-06-05 09:27:54 -0400

    created: users administration page

---
+++ pages/admin/users.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+import AdminUsers from '@/components/admin/AdminUsers'
+
+const users = () => {
+  return (
+    <div>
+      <AdminUsers />
+    </div>
+  )
+}
+
+export default users
